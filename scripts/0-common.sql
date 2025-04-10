a201140e64a38cc4f567abcef9ebf4dd3d78f227
Murrell Johnson <dev@klikz.us>
2025-04-10 13:07:36 -0400

    feat: question_lookup_tracking

---
+++ scripts/0-common.sql
@@ -0,0 +1,24 @@
+INSERT INTO `epic_prod`.`question_lookup_tracking` 
+(`study_id`, `is_answer`, `type`, `key`, `section_group`, `value`, `additional_info`, `order`, `required`, `double_columns`) 
+VALUES
+(2272, 0, 'QUESTION_TEXTAREA', 'COMMUNICATION_NOTES', 0, 'Communication Notes:', 'For each point of communication, you must document your initials, the date and time, the form of communication<br>whether it is a call, text or email, the person contacted, a summary of the communication that occurred, and any<br>transportation arrangements.', 1, 0, 0),
+(2272, 0, 'QUESTION_HEADER', 'SECTION_HEADER_1', 1, 'Study Site Screening Visit #1', '', 2, 0, 1),
+(2272, 0, 'QUESTION_DATETIME', 'DATE_SCHEDULED_1', 1, 'Date Scheduled to Present at Study Site: ', '', 3, 0, 1),
+(2272, 0, 'QUESTION_BOOLEAN', 'REQUIRE_TRANSPORT_1', 1, 'Does the patient require transportation?', '', 4, 0, 1),
+(2272, 0, 'QUESTION_DATE', 'DATE_PRESENTED_1', 1, 'Date Presented at Study Site:', '', 5, 0, 1),
+(2272, 0, 'QUESTION_HEADER', 'SECTION_HEADER_2', 2, 'Study Site Screening Visit #2', '', 6, 0, 1),
+(2272, 0, 'QUESTION_DATETIME', 'DATE_SCHEDULED_2', 2, 'Date Scheduled to Present at Study Site: ', '', 7, 0, 1),
+(2272, 0, 'QUESTION_BOOLEAN', 'REQUIRE_TRANSPORT_2', 2, 'Does the patient require transportation?', '', 8, 0, 1),
+(2272, 0, 'QUESTION_DATE', 'DATE_PRESENTED_2', 2, 'Date Presented at Study Site:', '', 9, 0, 1),
+(2272, 0, 'QUESTION_HEADER', 'SECTION_HEADER_3', 3, 'Study Site Screening Visit #3', '', 10, 0, 1),
+(2272, 0, 'QUESTION_DATETIME', 'DATE_SCHEDULED_3', 3, 'Date Scheduled to Present at Study Site: ', '', 11, 0, 1),
+(2272, 0, 'QUESTION_BOOLEAN', 'REQUIRE_TRANSPORT_3', 3, 'Does the patient require transportation?', '', 12, 0, 1),
+(2272, 0, 'QUESTION_DATE', 'DATE_PRESENTED_3', 3, 'Date Presented at Study Site:', '', 13, 0, 1),
+(2272, 0, 'QUESTION_HEADER', 'SECTION_HEADER_4', 4, 'Study Site Screening Visit #4', '', 14, 0, 1),
+(2272, 0, 'QUESTION_DATETIME', 'DATE_SCHEDULED_4', 4, 'Date Scheduled to Present at Study Site: ', '', 15, 0, 1),
+(2272, 0, 'QUESTION_BOOLEAN', 'REQUIRE_TRANSPORT_4', 4, 'Does the patient require transportation?', '', 16, 0, 1),
+(2272, 0, 'QUESTION_DATE', 'DATE_PRESENTED_4', 4, 'Date Presented at Study Site:', '', 17, 0, 1),
+(2272, 0, 'QUESTION_HEADER', 'SECTION_HEADER_5', 5, 'Study Site Screening Visit #5', '', 18, 0, 1),
+(2272, 0, 'QUESTION_DATETIME', 'DATE_SCHEDULED_5', 5, 'Date Scheduled to Present at Study Site: ', '', 19, 0, 1),
+(2272, 0, 'QUESTION_BOOLEAN', 'REQUIRE_TRANSPORT_5', 5, 'Does the patient require transportation?', '', 20, 0, 1),
+(2272, 0, 'QUESTION_DATE', 'DATE_PRESENTED_5', 5, 'Date Presented at Study Site:', '', 21, 0, 1);
